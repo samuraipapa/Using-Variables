66503ffb416d1515f4060062b4ca9f0e25a11cb5
samuraipapa <yury@pulsesensor.com>
2016-02-03 14:29:55 -0500

    all works

--- Using Variables/ViewController.swift
+++ Using Variables/ViewController.swift
@@ -9,10 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var counterLabel: UILabel!
 
+    var myNumber = 0
+    
+    @IBAction func add1(sender: UIButton) {
+    
+        myNumber = myNumber+1
+        printIntToCounterLabel(myNumber)
+    }
+    
+    @IBAction func add2(sender: UIButton) {
+        myNumber = myNumber+2
+        printIntToCounterLabel(myNumber)
+
+    }
+    
+    @IBAction func add5(sender: UIButton) {
+        myNumber = myNumber+5
+        printIntToCounterLabel(myNumber)
+
+    }
+    
+    @IBAction func minus1(sender: AnyObject) {
+        myNumber = myNumber-1
+        printIntToCounterLabel(myNumber)
+
+    }
+    
+    @IBAction func minus2(sender: UIButton) {
+        myNumber = myNumber-2
+        printIntToCounterLabel(myNumber)
+
+    }
+    @IBAction func minus5(sender: UIButton) {
+        myNumber = myNumber-5
+        printIntToCounterLabel(myNumber)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +61,17 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+    func printIntToCounterLabel(myInt: Int ){
+        
+            let myStringAsNumber = "\(myInt)"
+
+        counterLabel.text = myStringAsNumber
+        
+        
+    }
+    
+    
 }
 
