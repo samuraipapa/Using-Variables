0c1b87688954bdb2b6ab1533b885c6dffe78f3f1
samuraipapa <yury@pulsesensor.com>
2016-02-03 14:02:20 -0500

    Initial Commit

---
+++ Using Variables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Using Variables
+//
+//  Created by Yury Gitman on 2/3/16.
+//  Copyright © 2016 yg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
